7db2645b3cd8671607caf8d16148a5734258a0bb
ABECKCROW <mudskippermat@yahoo.co.jp>
2023-12-07 00:37:40 +0900

    GET単体テスト(例外)実装

--- src/test/java/com/abeck/ssfa/service/CompanyServiceImplTest.java
+++ src/test/java/com/abeck/ssfa/service/CompanyServiceImplTest.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -46,5 +47,15 @@ class CompanyServiceImplTest {
         CompanyEntity actual = companyServiceImpl.findCompanyById(1);
         assertThat(actual).isEqualTo(new CompanyEntity(1,"株式会社ABECK","03-1234-5678","東京都","千代田区","1-1-1","S"));
         verify(companyMapper, times(1)).findCompanyById(1);
+    }
+
+    @Test
+    public void 企業検索で存在しないIDを指定したときに例外がスローされること() {
+        doReturn(Optional.empty()).when(companyMapper).findCompanyById(99);
+
+        assertThatThrownBy(() -> companyServiceImpl.findCompanyById(99)).isInstanceOfSatisfying(CompanyNotFoundException.class, e -> {
+            assertThat(e.getMessage()).isEqualTo("Company Not Found");
+        });
+        verify(companyMapper, times(1)).findCompanyById(99);
     }
 }
