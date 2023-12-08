bddcadaa2991b8cbc005fe9b06a871f74c7938b8
ABECKCROW <mudskippermat@yahoo.co.jp>
2023-12-08 21:59:53 +0900

    sqlProviderの階層変更

--- src/main/java/com/abeck/ssfa/mapper/CompanyMapper.java
+++ src/main/java/com/abeck/ssfa/mapper/CompanyMapper.java
@@ -1,12 +1,10 @@
 package com.abeck.ssfa.mapper;
 
-import com.abeck.ssfa.CompanySqlProvider;
 import com.abeck.ssfa.entity.CompanyEntity;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.SelectProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 import java.util.Optional;
--- src/main/java/com/abeck/ssfa/mapper/CompanySqlProvider.java
+++ src/main/java/com/abeck/ssfa/mapper/CompanySqlProvider.java
@@ -1,4 +1,4 @@
-package com.abeck.ssfa;
+package com.abeck.ssfa.mapper;
 
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.jdbc.SQL;
