983e94cef93687c71125c4f92c1f953bc95c3ed8
ABECKCROW <mudskippermat@yahoo.co.jp>
2023-12-08 12:17:12 +0900

    プロバイダークラスの実装

---
+++ src/main/java/com/abeck/ssfa/CompanySqlProvider.java
@@ -0,0 +1,40 @@
+package com.abeck.ssfa;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.jdbc.SQL;
+
+public class CompanySqlProvider {
+
+    public String SelectCompanyWithFilter(
+            @Param("companyName") String companyName,
+            @Param("companyPhone") String companyPhone,
+            @Param("region") String region,
+            @Param("city") String city,
+            @Param("rank") String rank
+    ) {
+        return new SQL() {{
+            SELECT("*");
+            FROM("your_table");
+
+            if (companyName != null && !companyName.isEmpty()) {
+                WHERE("company_name = #{companyName}");
+            }
+
+            if (companyPhone != null && !companyPhone.isEmpty()) {
+                WHERE("company_phone LIKE CONCAT('%', #{companyPhone}, '%')");
+            }
+
+            if (region != null && !region.isEmpty()) {
+                WHERE("region LIKE CONCAT('%', #{region}, '%')");
+            }
+
+            if (city != null && !city.isEmpty()) {
+                WHERE("city LIKE CONCAT('%', #{city}, '%')");
+            }
+
+            if (rank != null && !rank.isEmpty()) {
+                WHERE("rank LIKE CONCAT('%', #{rank}, '%')");
+            }
+        }}.toString();
+    }
+}
