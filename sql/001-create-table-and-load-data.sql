c71cc1ab6723832bee0999d28e923611a1e579a3
ABECKCROW <mudskippermat@yahoo.co.jp>
2023-12-08 11:09:21 +0900

    sqlの初期データ変更

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -4,50 +4,49 @@ DROP TABLE IF EXISTS histories;
 DROP TABLE IF EXISTS appointments;
 
 CREATE TABLE sales_persons (
-    sales_person_id unsigned AUTO_INCREMENT,
-    sales_person_name VARCHAR NOT NULL,
+    sales_person_id INT unsigned AUTO_INCREMENT,
+    sales_person_name VARCHAR(10) NOT NULL,
     PRIMARY KEY(sales_person_id)
 );
 
 CREATE TABLE companies (
-    company_id SERIAL unsigned AUTO_INCREMENT,
-    company_name VARCHAR　NOT NULL,
-    company_region VARCHAR(4)　NOT NULL,
-    company_city VARCHAR　NOT NULL,
-    company_address VARCHAR,
-    company_phone INTEGER　NOT NULL,
+    company_id INT unsigned AUTO_INCREMENT,
+    company_name VARCHAR(40) NOT NULL,
+    company_phone VARCHAR(12) NOT NULL,
+    company_region VARCHAR(4) NOT NULL,
+    company_city VARCHAR(6) NOT NULL,
+    company_address VARCHAR(40),
     company_rank CHAR(1),
-    sales_person_id INTEGER NOT NULL,
+    sales_person_id INT unsigned NOT NULL,
     PRIMARY KEY(company_id),
     FOREIGN KEY(sales_person_id) REFERENCES sales_persons(sales_person_id)
 );
 
 CREATE TABLE histories (
-    history_id SERIAL unsigned AUTO_INCREMENT,
+    history_id INT unsigned AUTO_INCREMENT,
     history_date TIMESTAMP NOT NULL,
-    approach_type VARCHAR NOT NULL,
-    approach_results VARCHAR NOT NULL,
-    company_id　INTEGER　NOT NULL,
+    approach_type VARCHAR(10) NOT NULL,
+    approach_results TEXT,
+    company_id INT unsigned NOT NULL,
     PRIMARY KEY(history_id),
-    FOREIGN KEY(company_id) REFERENCES companies(company_id),
+    FOREIGN KEY(company_id) REFERENCES companies(company_id)
 );
 
 CREATE TABLE appointments (
-    appointment_id unsigned AUTO_INCREMENT,
+    appointment_id INT unsigned AUTO_INCREMENT,
     appointment_date TIMESTAMP NOT NULL,
-    history_id　INTEGER　NOT NULL,
+    history_id INT unsigned NOT NULL,
     PRIMARY KEY(appointment_id),
     FOREIGN KEY(history_id) REFERENCES histories(history_id)
 );
 
-INSERT INTO sales_persons (sales_person_name) VALUES
-('安部'), ('山田'), ('佐藤'), ('鈴木');
+INSERT INTO sales_persons (sales_person_name) VALUES('安部'),('山田'),('佐藤'),('鈴木');
 
-INSERT INTO companies (company_name, company_region, company_city, company_phone, company_rank, sales_person_id) VALUES
-('ABECK株式会社', '神奈川県', '川崎市', 1234567890, "S", 1),
-('Disk株式会社', '神奈川県', '厚木市', 1234567890, "A", 1),
-('マウンテン株式会社', '神奈川県', '箱根町', 1234567890, "B", 2),
-('シュガー株式会社', '神奈川県', '綾瀬市', 1234567890, "C", 3);
+INSERT INTO companies (company_name, company_phone, company_region, company_city, company_address, company_rank, sales_person_id) VALUES
+("ABECK株式会社", "03-1234-5678","神奈川県", "川崎市",  "1-1-1", "S", 1),
+("Disk株式会社", "046-123-4567","神奈川県", "厚木市",  "2-2-2", "A", 2),
+("マウンテン株式会社", "0460-12-3456","神奈川県", "箱根町",  "3-3-3", "B", 3),
+("シュガー株式会社", "0467-12-3456","神奈川県", "綾瀬市",  "4-4-4", "C", 4);
 
 INSERT INTO histories (history_date, approach_type, approach_results, company_id) VALUES
 ('2023-11-10', '架電', '担当者不在', 1),
