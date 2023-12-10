9b8cd396c6bb0d5ee3ce0f5afc284f2bc2d6b967
ABECKCROW <mudskippermat@yahoo.co.jp>
2023-12-10 13:51:30 +0900

    クエリ指定時の結合テストを実装

--- src/test/java/integrationtest/CompanyRestApiIntegrationTest.java
+++ src/test/java/integrationtest/CompanyRestApiIntegrationTest.java
@@ -77,6 +77,111 @@ public class CompanyRestApiIntegrationTest {
     @Test
     @DataSet(value = "datasets/companies.yml")
     @Transactional
+    void クエリパラメータの指定した企業名と部分一致する企業情報が取得できること() throws Exception {
+        String response = mockMvc.perform(MockMvcRequestBuilders.get("/companies?companyName=ABECK"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        JSONAssert.assertEquals("[" +
+                "{" +
+                "    \"companyId\": 1," +
+                "    \"companyName\": \"ABECK株式会社\"," +
+                "    \"companyPhone\": \"03-1234-5678\"," +
+                "    \"region\": \"東京都\"," +
+                "    \"city\": \"千代田区\"," +
+                "    \"address\": \"1-1-1\"," +
+                "    \"companyRank\": \"S\"" +
+                "}" +
+                "]", response, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    @DataSet(value = "datasets/companies.yml")
+    @Transactional
+    void クエリパラメータの指定した電話番号と完全一致する企業情報が取得できること() throws Exception {
+        String response = mockMvc.perform(MockMvcRequestBuilders.get("/companies?companyPhone=03-1234-5678"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        JSONAssert.assertEquals("[" +
+                "{" +
+                "    \"companyId\": 1," +
+                "    \"companyName\": \"ABECK株式会社\"," +
+                "    \"companyPhone\": \"03-1234-5678\"," +
+                "    \"region\": \"東京都\"," +
+                "    \"city\": \"千代田区\"," +
+                "    \"address\": \"1-1-1\"," +
+                "    \"companyRank\": \"S\"" +
+                "}" +
+                "]", response, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    @DataSet(value = "datasets/companies.yml")
+    @Transactional
+    void クエリパラメータの指定した都道府県と完全一致する企業情報が取得できること() throws Exception {
+        String response = mockMvc.perform(MockMvcRequestBuilders.get("/companies?region=東京都"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        JSONAssert.assertEquals("[" +
+                "{" +
+                "    \"companyId\": 1," +
+                "    \"companyName\": \"ABECK株式会社\"," +
+                "    \"companyPhone\": \"03-1234-5678\"," +
+                "    \"region\": \"東京都\"," +
+                "    \"city\": \"千代田区\"," +
+                "    \"address\": \"1-1-1\"," +
+                "    \"companyRank\": \"S\"" +
+                "}" +
+                "]", response, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    @DataSet(value = "datasets/companies.yml")
+    @Transactional
+    void クエリパラメータの指定した市区町村と完全一致する企業情報が取得できること() throws Exception {
+        String response = mockMvc.perform(MockMvcRequestBuilders.get("/companies?city=千代田区"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        JSONAssert.assertEquals("[" +
+                "{" +
+                "    \"companyId\": 1," +
+                "    \"companyName\": \"ABECK株式会社\"," +
+                "    \"companyPhone\": \"03-1234-5678\"," +
+                "    \"region\": \"東京都\"," +
+                "    \"city\": \"千代田区\"," +
+                "    \"address\": \"1-1-1\"," +
+                "    \"companyRank\": \"S\"" +
+                "}" +
+                "]", response, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    @DataSet(value = "datasets/companies.yml")
+    @Transactional
+    void クエリパラメータの指定した企業ランクと完全一致する企業情報が取得できること() throws Exception {
+        String response = mockMvc.perform(MockMvcRequestBuilders.get("/companies?companyRank=S"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        JSONAssert.assertEquals("[" +
+                "{" +
+                "    \"companyId\": 1," +
+                "    \"companyName\": \"ABECK株式会社\"," +
+                "    \"companyPhone\": \"03-1234-5678\"," +
+                "    \"region\": \"東京都\"," +
+                "    \"city\": \"千代田区\"," +
+                "    \"address\": \"1-1-1\"," +
+                "    \"companyRank\": \"S\"" +
+                "}" +
+                "]", response, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    @DataSet(value = "datasets/companies.yml")
+    @Transactional
     void 企業検索で存在するIDを指定したときに正常に企業情報が取得できること() throws Exception {
         String response = mockMvc.perform(MockMvcRequestBuilders.get("/companies/1"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
